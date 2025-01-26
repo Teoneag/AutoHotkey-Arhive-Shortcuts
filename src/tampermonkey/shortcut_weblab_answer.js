d817fb20a44679cd721b5ae6c6755dd62a07fb7c
Teoneag <teodor.neagoe.climber@gmail.com>
2025-01-26 14:56:11 +0100

    ✨ weblab answer unlock all shortcuts from one page

--- src/tampermonkey/shortcut_weblab_answer.js
+++ src/tampermonkey/shortcut_weblab_answer.js
@@ -18,11 +18,15 @@
         const newTab = window.open(requestUrl, '_blank');
         await new Promise((resolve) => newTab.onload = resolve); // Wait for the new tab to load
 
-        // click the remove lock button
-        const anchor = [...newTab.document.body.getElementsByTagName('a')].find(
+        // Find all the elements that have the 'Yes, remove lock' text
+        const anchors = [...newTab.document.body.getElementsByTagName('a')].filter(
             a => a.textContent.includes('🔓 Yes, remove lock')
         );
-        anchor.click();
+
+        // Click on each of the found elements to remove the lock
+        for (let anchor of anchors) {
+            anchor.click();
+        }
 
         // close the new tab and refresh the original tab
         await new Promise(resolve => setTimeout(resolve, 1)); // await for the lock to be removed
